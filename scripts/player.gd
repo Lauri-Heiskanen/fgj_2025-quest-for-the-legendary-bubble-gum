2796d67a0ae18a27473af0a4720eb3383f9f4eac
LatexGlovesLmao <106094206+Lauri-Heiskanen@users.noreply.github.com>
2025-01-25 00:46:37 +0200

    turning works and began working on movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,9 +7,12 @@ const JUMP_VELOCITY = 4.5
 @onready var head : MeshInstance3D = $Head
 @onready var cam_first : Camera3D = $Head/First
 
-var is_stationary = true
+var is_moving: bool = false
+var is_rotating: bool = false
 var delta_sum: float = 0
-var target_rot = Quaternion(transform.basis)
+
+var target_rot: Quaternion = Quaternion(transform.basis)
+var target_pos: Transform3D
 
 var motion : int = 0
 var turn : int = 0
@@ -21,41 +24,60 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	
-	if !is_stationary:
-		delta_sum += delta
+	if Input.is_action_pressed("forward"):
+		if !is_rotating && !is_moving:
+			if Quaternion(transform.basis) == Quaternion(0, 0, 0, 1).normalized(): # facing front
+				is_moving = true
+				delta_sum = 0
+				target_pos = transform
+			elif Quaternion(transform.basis) == Quaternion(0, 1, 0, 0).normalized(): # faciong back
+				is_moving = true
+				delta_sum = 0
+				target_pos = transform
+				
+			
+			
+
+	elif Input.is_action_pressed("backward"):
+		if !is_rotating && !is_moving:
+			# is_moving = true
+			delta_sum = 0
+			print("backward")
+		
+	elif Input.is_action_pressed("left"):
+		if !is_rotating && !is_moving:
+			is_rotating = true
+			delta_sum = 0
+			target_rot = (Quaternion(Vector3(0, 1, 0), deg_to_rad(90)).normalized() * Quaternion(transform.basis)).normalized()
+			
+	elif Input.is_action_pressed("right"):
+		if !is_rotating && !is_moving:
+			is_rotating = true
+			delta_sum = 0
+			target_rot = (Quaternion(Vector3(0, -1, 0), deg_to_rad(90)).normalized() * Quaternion(transform.basis)).normalized()
+	
+	if is_rotating:
+		delta_sum += delta * 2 # rotation speed multiplier
+		if delta_sum > 1:
+			delta_sum = 1
+	elif is_moving:
+		delta_sum += delta  # movement speed multiplier
 		if delta_sum > 1:
 			delta_sum = 1
 	
-	if Input.is_action_pressed("forward"):
-		print("forward")
-
-	elif Input.is_action_pressed("backward"):
-		print("backward")
-		
-	elif Input.is_action_pressed("left"):
-		if is_stationary:
-			is_stationary = false
-			delta_sum = 0
-			target_rot = (Quaternion(transform.basis) * Quaternion(0, 1, 0, 1)).normalized()
-			print("Left")
+	if is_rotating:
+		if delta_sum == 1:
+			transform.basis = Basis(target_rot)
+			is_rotating = false
+		elif delta_sum < 1:
+			transform.basis = Basis(Quaternion(transform.basis).slerp(target_rot, delta_sum))
 			
-	elif Input.is_action_pressed("right"):
-		if is_stationary:
-			is_stationary = false
-			delta_sum = 0
-			target_rot = (Quaternion(transform.basis) * Quaternion(0, -1, 0, 1)).normalized()
-			print("Right")
-	
-	transform.basis = Basis(Quaternion(transform.basis).slerp(target_rot, delta_sum))
-	
-	if !is_stationary:
-		if target_rot == Quaternion(transform.basis):
-			is_stationary = true
-			
-		else:
-			print(target_rot)
-			print(Quaternion(transform.basis))
-			print()
+	if is_moving:
+		if delta_sum == 1:
+			transform.basis = Basis(target_rot)
+			is_rotating = false
+		elif delta_sum < 1:
+			transform.basis = Basis(Quaternion(transform.basis).slerp(target_rot, delta_sum))
 
 
 	move_and_slide()
--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -3,17 +3,20 @@ extends Control
 var health : int = 100
 
 @onready var lbl_hp : Label = $MarginContainer/Health
-@onready var lbl_player_stationary : Label = $MarginContainer2/is_stationary
+@onready var lbl_player_is_rotating : Label = $MarginContainer2/VBoxContainer/is_rotating
+@onready var lbl_player_is_moving : Label = $MarginContainer2/VBoxContainer/is_moving
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	lbl_hp.text = "HP: " + str(health)
-	lbl_player_stationary = $MarginContainer2/is_stationary
-	lbl_player_stationary.text = "is_stationary: " + str(get_parent().is_stationary)
-	
-
+	lbl_player_is_rotating = $MarginContainer2/VBoxContainer/is_rotating
+	lbl_player_is_rotating.text = "is_rotating: " + str(get_parent().is_rotating)
+	lbl_player_is_moving = $MarginContainer2/VBoxContainer/is_moving
+	lbl_player_is_moving.text = "is_moving: " + str(get_parent().is_moving)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	lbl_player_is_rotating.text = "is_rotating: " + str(get_parent().is_rotating)
+	lbl_player_is_moving.text = "is_moving: " + str(get_parent().is_moving)
+	
