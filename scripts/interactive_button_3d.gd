da99537007a3d8de171c68585d018e9c88c02238
Dorfultariant <tunxero@hotmail.com>
2025-01-25 16:23:27 +0200

    Signals for state and items

--- scripts/interactive_button_3d.gd
+++ scripts/interactive_button_3d.gd
@@ -1,16 +1,22 @@
 # Button3D.gd
 extends StaticBody3D
 
+signal send_item(item: String)
+signal send_button_state(id, state: bool)
+
+
 @onready var control_btn = $MeshInstance3D/ObjBtn/SubViewportContainer/SubViewport/Button
 @onready var viewport = $MeshInstance3D/ObjBtn/SubViewportContainer/SubViewport
 @onready var obj_btn = $MeshInstance3D/ObjBtn
 @onready var animPlayer = $AnimationPlayer
-@export var button_action : String = ""
-@export var toggle : bool
+@export var is_item : bool = false
+@export var item : String = ""
+@export var toggle : bool = false
+@export var id : int
 
 var toggle_state : bool = false
-var cnt = 0
 
+# Src used : https://forum.godotengine.org/t/any-ideas-on-how-make-an-interactible-button-in-a-3d-space/4366/2
 func _ready():
 	connect("input_event", Callable(self, "_on_Button3D_input_event"), 0)
 	control_btn.connect("pressed", Callable(self, "_on_Button_pressed"), 0)
@@ -33,3 +39,9 @@ func _on_Button_pressed():
 		toggle_state = !toggle_state
 	else:
 		animPlayer.play("press_down")
+
+	if is_item:
+		visible = false
+		send_item.emit(item)
+	else:
+		send_button_state.emit(id, toggle_state)
