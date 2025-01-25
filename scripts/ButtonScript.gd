9e2dbfa32f2607dd2507cd8fa85606d1c01f8501
Dorfultariant <tunxero@hotmail.com>
2025-01-25 22:19:22 +0200

    Refactoring button, item scripts

--- scripts/ButtonScript.gd
+++ scripts/ButtonScript.gd
@@ -1,20 +1,23 @@
-# Button3D.gd
 extends StaticBody3D
 
+class_name ButtonScript
 
-signal send_item(item: String)
+signal send_button_state(id : int, toggle_state: bool)
 
-@onready var control_btn = $MeshInstance3D/ObjBtn/SubViewportContainer/SubViewport/Button
-@onready var viewport = $MeshInstance3D/ObjBtn/SubViewportContainer/SubViewport
+@onready var control_btn = $SubViewportContainer/SubViewport/Button
+@onready var viewport = $SubViewportContainer/SubViewport
 @onready var animPlayer = $AnimationPlayer
-@export var item : String = ""
 
+@export var button_action : String = ""
+@export var id : int = 0
+@export var toggle : bool
 
-# Src used : https://forum.godotengine.org/t/any-ideas-on-how-make-an-interactible-button-in-a-3d-space/4366/2
+var toggle_state : bool = false
+
 func _ready():
 	connect("input_event", Callable(self, "_on_Button3D_input_event"), 0)
 	control_btn.connect("pressed", Callable(self, "_on_Button_pressed"), 0)
-	
+
 
 func _on_Button3D_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton:
@@ -25,6 +28,10 @@ func _on_Button3D_input_event(_camera, event, _position, _normal, _shape_idx):
 			control_btn.set_pressed(false)
 
 func _on_Button_pressed():
-	print("Button press")
-	visible = false
-	send_item.emit(item)
+	if toggle_state:
+		animPlayer.play_backwards("action")
+	else:
+		animPlayer.play("action")
+
+	toggle_state = !toggle_state
+	send_button_state.emit(id, toggle_state)
