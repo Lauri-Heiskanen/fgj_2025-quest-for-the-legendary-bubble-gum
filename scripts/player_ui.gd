e9e2895cb20c088ae11f4948fa4fd8c29ea5ff92
Dorfultariant <tunxero@hotmail.com>
2025-01-26 13:19:13 +0200

    Level testing

--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -2,10 +2,11 @@ extends Control
 
 @onready var hotbar : HBoxContainer = $hotbar_container/hotbar
 @onready var slots : Array = hotbar.get_children()
+@onready var tl : ColorRect = $TransitionLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	tl.color = Color(25,22,21,0)
 	var i : int = 0
 	while i < 4:
 		var item = get_parent().player_items[i]
@@ -19,4 +20,3 @@ func _on_player_items_changed(index, new_item):
 		slots[index].get_children()[1].texture = load("res://uiTextures/" + new_item + ".png")
 	else:
 		slots[index].get_children()[1].texture = null
-		
